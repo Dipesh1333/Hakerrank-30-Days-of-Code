5c90ad920e739925af080c7b76d96c500164f21a
Dipesh1333 <44140636+Dipesh1333@users.noreply.github.com>
2020-12-26 22:25:50 +0530

    Add files via upload

---
+++ Day 1 Hello World.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"Hello, World."<<endl;
+    string s;
+    getline(cin,s);
+    cout<<s;
+
+    return 0;
+}
