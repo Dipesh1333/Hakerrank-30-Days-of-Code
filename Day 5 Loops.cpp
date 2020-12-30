888a0550f017d5ea6d17725b5e2e8bc7d456ccf6
Dipesh1333 <44140636+Dipesh1333@users.noreply.github.com>
2020-12-30 18:03:41 +0530

    Add files via upload

---
+++ Day 5 Loops.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, n;
+    cin>>n;
+    for(i=1;i<11;i++){
+        cout<< n << " x " << i << " = " << n*i <<endl;
+    }
+    
+    
+    return 0;
+}
