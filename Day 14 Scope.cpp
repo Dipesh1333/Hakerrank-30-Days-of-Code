f02f066efbd9d63bfd4dca340f95b248539e691a
Dipesh1333 <44140636+Dipesh1333@users.noreply.github.com>
2021-01-05 11:51:39 +0530

    Add files via upload

---
+++ Day 14 Scope.cpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+Day 14: Scope
+
+*******************************************************************************/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+     Difference (vector<int> a){
+         elements = a;
+     }
+     
+     void computeDifference(){
+         maximumDifference=0;
+         
+         for(int i=0;i<elements.size();i++){
+             for(int j=i+1;j<elements.size();j++){
+                 if(abs(elements[i]-elements[j]) > maximumDifference){
+                     maximumDifference = abs(elements[i]-elements[j]);
+                 }
+             }
+         }
+     }
+     
+    // Add your code here
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
